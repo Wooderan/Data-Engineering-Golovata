8cc682b52db3ba0c41a91f8e23a2855172daef3e
user <user@mail.com>
2023-12-03 11:56:50 +0200

    Implement Job2. Parse raw sales data in local folder and convert it to avro folder

---
+++ lesson02/ht_template/job2/main.py
@@ -0,0 +1,41 @@
+"""
+This file contains the controller that accepts command via HTTP
+and trigger business logic layer
+"""
+import os
+from flask import Flask, request
+from flask import typing as flask_typing
+
+from lesson02.ht_template.job2.bll.sales_api import stage_sales_on_local_disk
+
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['POST'])
+def main() -> flask_typing.ResponseReturnValue:
+    """
+    Controller that accepts command via HTTP and
+    trigger business logic layer
+
+    Proposed POST body in JSON:
+    {
+      "date: "2022-08-09",
+      "raw_dir": "/path/to/my_dir/raw/sales/2022-08-09"
+    }
+    """
+    input_data: dict = request.json
+    # TODO: implement me
+    raw_dir = input_data.get('raw_dir')
+    stg_dir = input_data.get('stg_dir')
+
+    stage_sales_on_local_disk(raw_dir=raw_dir, stg_dir=stg_dir)
+
+    return {
+               "message": "Data retrieved successfully from API",
+           }, 201
+
+
+if __name__ == "__main__":
+    # app.run(debug=True, host="localhost", port=8081)
+    app.run(port=8082)
